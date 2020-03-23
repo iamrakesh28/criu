b253b0235ab0e805cece36bbd5066831d1b511a1
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2020-03-23 23:00:19 +0530

    pagemap added and __rhandler removed
    handler for anonymising pagemap img file added. __rhandler() was not required and therefore image module import not required

--- lib/py/anonymize.py
+++ lib/py/anonymize.py
@@ -1,35 +1,14 @@
 import struct
 
-from images import magic
-from images import MagicException
-
-def shred():
-    print("test message")
+import pagemap
     
 handlers = {
-    'INVENTORY': shred
+    'PAGEMAP': pagemap.shred
 }
 
-def __rhandler(f):
-    # Images v1.1 NOTE: First read "first" magic.
-    img_magic, = struct.unpack('i', f.read(4))
-    if img_magic in (magic.by_name['IMG_COMMON'],
-                     magic.by_name['IMG_SERVICE']):
-        img_magic, = struct.unpack('i', f.read(4))
 
-    try:
-        m = magic.by_val[img_magic]
-    except:
-        raise MagicException(img_magic)
-
-    try:
-        handler = handlers[m]
-    except:
-        raise Exception("No handler found for image with magic " + m)
-
-    return m, handler
-
-def main(f):
-    images = {}
-    m, handler = __rhandler(f)
-    handler()
+def main(image):
+    magic = image['magic']
+    entries = handlers[magic](image['entries'])
+    image['entries'] = entries
+    return images
