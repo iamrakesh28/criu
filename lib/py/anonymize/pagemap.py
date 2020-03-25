0a995b57a8021c8a0e9b95f1029471abac3af02d
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2020-03-25 09:58:16 +0530

    anonymize directory deleted

--- lib/py/anonymize/pagemap.py
+++
@@ -1,8 +0,0 @@
-# shred sets virtual addresses to 0
-
-def shred(img):
-        for entry in img:
-                if 'vaddr' in entry:
-                        entry['vaddr'] = 0
-
-        return img
